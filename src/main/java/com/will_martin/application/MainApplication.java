cfa05de8ecc2890bda0a50f7f90520a59796b94b
William Martin <will.st4@gmail.com>
2024-09-17 20:31:01 +0100

    Adds generated annotation

--- src/main/java/com/will_martin/application/MainApplication.java
+++ src/main/java/com/will_martin/application/MainApplication.java
@@ -1,10 +1,12 @@
 package com.will_martin.application;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
+    @Generated
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
